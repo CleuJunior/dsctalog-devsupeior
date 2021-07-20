92c04a2220ad5a78662891a3d320d7979d136e7d
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-07-20 20:12:05 -0300

    first commit

---
+++ src/main/java/com/devsuperior/dsctalog/DsctalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsctalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsctalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsctalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsctalog/DsctalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsctalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsctalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
