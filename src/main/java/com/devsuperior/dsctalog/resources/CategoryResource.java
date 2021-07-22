57223b9d43ccc901f770c05706ef99dcf9e653c8
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-07-21 21:33:34 -0300

    FindById

--- src/main/java/com/devsuperior/dsctalog/resources/CategoryResource.java
+++ src/main/java/com/devsuperior/dsctalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import com.devsuperior.dsctalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,14 @@ public class CategoryResource {
     {
         List<CategoryDTO> list = categoryService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findByID(@PathVariable Long id)
+    {
+        CategoryDTO dto = categoryService.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 
 }
--- src/main/java/com/devsuperior/dsctalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dsctalog/services/CategoryService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,5 +24,12 @@ public class CategoryService {
         List<Category> list = categoryRepository.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+       Optional<Category> obj = categoryRepository.findById(id);
+       Category entity = obj.get();
+       return new CategoryDTO(entity);
     }
 }
