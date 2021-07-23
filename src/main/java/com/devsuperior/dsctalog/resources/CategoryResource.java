83356654f424fa8198783adce637e2817ca4de43
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-07-22 21:42:05 -0300

    POST new Category

--- src/main/java/com/devsuperior/dsctalog/resources/CategoryResource.java
+++ src/main/java/com/devsuperior/dsctalog/resources/CategoryResource.java
@@ -4,11 +4,10 @@ import com.devsuperior.dsctalog.dto.CategoryDTO;
 import com.devsuperior.dsctalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,5 +32,15 @@ public class CategoryResource {
         CategoryDTO dto = categoryService.findById(id);
         return ResponseEntity.ok().body(dto);
     }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto)
+    {
+        dto = categoryService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
 
 }
--- src/main/java/com/devsuperior/dsctalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dsctalog/services/CategoryService.java
@@ -33,4 +33,13 @@ public class CategoryService {
        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
        return new CategoryDTO(entity);
     }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = categoryRepository.save(entity);
+
+        return new CategoryDTO(entity);
+    }
 }
