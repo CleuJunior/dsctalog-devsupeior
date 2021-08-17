84031ad63235986c5a027a0f813d22db593095b4
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2021-08-17 09:08:22 -0300

    FindAll camada Web e Teste ded integracao findAllPaged

---
+++ src/test/java/com/devsuperior/dsctalog/resources/ProductResourceIT.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dsctalog.resources;
+
+import com.devsuperior.dsctalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach()
+    void setup() throws Exception {
+        existingId = 1L;
+        nonExistingId = 2333L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void findAllShouldReturnSortedPageWhenSortByName() throws Exception{
+        ResultActions result = mockMvc.perform(get("/products?page=0&size=12&sort=name,asc").accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+        result.andExpect(jsonPath("$.content").exists());
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+        result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+    }
+}
--- src/test/java/com/devsuperior/dsctalog/services/ProductServiceIT.java
+++ src/test/java/com/devsuperior/dsctalog/services/ProductServiceIT.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsctalog.services;
 
+import com.devsuperior.dsctalog.dto.ProductDTO;
 import com.devsuperior.dsctalog.repositories.ProductRepository;
 import com.devsuperior.dsctalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -7,8 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
     @Autowired
@@ -37,9 +43,40 @@ public class ProductServiceIT {
 
     @Test
     public void deleteShouldThrowsResourceNotFoundExceptionWhenIdNotExists(){
-        productService.delete(nonExistingId);
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             productService.delete(nonExistingId);
         });
+    }
+
+    @Test
+    public void findAllPagedShouldReturnPageWhenPage0Size10(){
+        PageRequest pageRequest = PageRequest.of(0,10);
+        Page<ProductDTO> result = productService.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber());
+        Assertions.assertEquals(10, result.getSize());
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist(){
+        PageRequest pageRequest = PageRequest.of(50,10);
+        Page<ProductDTO> result = productService.findAllPaged(pageRequest);
+
+        Assertions.assertTrue(result.isEmpty());
+
+    }
+
+    @Test
+    public void findAllPagedShouldReturnSortedPageWhenSortByName(){
+        PageRequest pageRequest = PageRequest.of(0,10, Sort.by("name"));
+        Page<ProductDTO> result = productService.findAllPaged(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+
     }
 }
