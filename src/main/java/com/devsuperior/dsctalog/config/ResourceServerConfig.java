2603be061efe806d4dc2c5775b251ed62347eeff
CleuJunior <cleu.junior@gmail.com>
2021-08-29 11:50:35 -0300

    03-26 Implementando OAuth2 resource server

---
+++ src/main/java/com/devsuperior/dsctalog/config/ResourceServerConfig.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dsctalog.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+
+    private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
+
+    private static final String[] ADMIN = { "/users/**" };
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception { resources.tokenStore(tokenStore); }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        // H2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
+                .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+}
+
